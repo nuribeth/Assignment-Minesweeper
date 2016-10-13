3474171e13a11521b7a642df5ab3b119979e389f
nuribeth.valle <nuribeth.valle@10.33.60.185>
2016-10-13 10:09:10 -0400

    First Commit

---
+++ src/minesweeper/Game.java
@@ -0,0 +1,15 @@
+package minesweeper;
+
+public class Game {
+	private Cell [][] cells;
+	private int CellID= 0;
+	private int side= 9;
+	private int limit= side-1;
+	
+	public void setGame() {
+		
+	}
+	
+			
+
+}
